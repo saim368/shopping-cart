ad695825e7548b8db301399e5ba075d4d094c10c
saim368 <saimharoon62@gmail.com>
2020-10-15 16:18:34 -0400

    filter redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 //feature 1
 import React from 'react';
-import data from './data.json';
 import Products from './components/Products';
 import Filter from './components/Filter';
 import Cart from './components/Cart';
@@ -12,7 +11,6 @@ class App extends React.Component {
   constructor () {
     super ();
     this.state = {
-      products: data.products,
       cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
       size: "",
       sort: "",
